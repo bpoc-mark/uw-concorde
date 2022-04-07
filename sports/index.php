fe66d085b25ae1db63e8353dd5e92a3cc947e7b8
bpoc-mark <markariel.maata@bpoc.co.jp>
2022-04-07 13:22:28 +0800

    added sports page pc and sp view

---
+++ sports/index.php
@@ -0,0 +1,233 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/inc/config.php");?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
+        <meta name="robots" content="index,follow">
+        <link rel="canonical" href="<?php echo CANONICAL ?>">
+        <meta name="keywords" content="<?php echo KEY_WORD ?>">
+        <meta name="description" content="<?php echo DESCRIPTION ?>">
+        <title>朝食バイキング｜<?php echo SITE_NAME ?></title>
+        <meta property="og:title" content="朝食バイキング｜<?php echo SITE_NAME ?>">
+        <meta property="og:type" content="website">
+        <meta property="og:description" content="<?php echo DESCRIPTION ?>">
+        <meta property="og:url" content="<?php echo CANONICAL ?>">
+        <meta property="og:image" content="/images/fb_ogp-image.jpg">
+        <?php include($_SERVER['DOCUMENT_ROOT']."/inc/header_include.php");?>
+        <link href="/css/sports.css" media="print" rel="stylesheet">
+        <link href="/css/sports.css" media="screen and (min-width: 801px)" rel="stylesheet">
+        <link href="/css/sports-sp.css" media="only screen and (max-width: 800px)" rel="stylesheet">
+        <?php include($_SERVER['DOCUMENT_ROOT']."/inc/ga.php");?>
+        <?php include($_SERVER['DOCUMENT_ROOT']."/inc/tag/header_tag.php");?>
+    </head>
+    <body>
+        <?php include($_SERVER['DOCUMENT_ROOT']."/inc/tag/content_tag.php");?>
+        <div id="AllBox">
+            <!--======================== HeaderBox ===============================================-->
+            <div id="HeaderBox">
+                <?php include($_SERVER['DOCUMENT_ROOT']."/inc/header_box.php");?>
+            </div>
+            <!-- HeaderBox -->
+            <!--======================== //HeaderBox ===============================================-->
+            <!--======================== Contents ===============================================-->
+            <div id="Contents" class="cf">
+                <!--======================== Main =====================================-->
+                <div id="Main">
+                    <div class="main_img_detail">
+                        <div class="tit">
+                            <p class="cap"></p>
+                            <h1 class="sawa">スポーツ・団体合宿 </h1>
+                            <p class="sub incon">SPORTS CAMP</p>
+                        </div>
+                        <!--======================== topic_path =====================================-->
+                        <div class="topic_path">
+                            <ul>
+                                <li class="home"><a href="/">浜松のホテルコンコルド浜松</a></li>
+                                <li>スポーツ・団体合宿</li>
+                            </ul>
+                        </div>
+                        <!--======================== //topic_path =====================================-->
+                        <div class="bg bg_top bg_breakfast01"></div>
+                    </div>
+					<div class="sports-content">
+                        <section class="sect-1">
+                            <div class="wrapper">
+                                <h2 class="bef_01 sawa">浜松のスポーツ・<br class="sp">団体合宿なら、<br>ホテルコンコルド浜松</h2>
+                                <p>プロ野球、Jリーグなどのプロスポーツチームのほか<br class="pc">大学生・高校生の合宿や海外からの遠征も多数受け入れ経験あり！</p>
+                                <div class="g_box01">
+                                    <div class="content">
+                                        <h2 class="def sawa">主要スポーツ会場への<br class="sp">アクセスも抜群！</h2>
+                                        <div class="row">
+                                            <div class="col">
+                                                <picture>
+                                                    <source srcset="/images/sports/sp/sect_1_img_1.jpg" media="(max-width: 899px)">
+                                                    <img src="/images/sports/sect_1_img_1.jpg" alt="">
+                                                </picture>
+                                            </div>
+                                            <div class="col">
+                                                <p>- 施設と移動時間</p>
+                                                <table>
+                                                    <tr>
+                                                        <th>施設</th>
+                                                        <th>移動時間（車）</th>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>エコパ</td>
+                                                        <td>約50分</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>さわやかアリーナ</td>
+                                                        <td>約40分</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>ToBiO</td>
+                                                        <td>約30分</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>花川運動公園</td>
+                                                        <td>約30分</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>浜松アリーナ</td>
+                                                        <td>約20分</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>ヤマハスタジアム</td>
+                                                        <td>約40分</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>四ツ池公園<br class="sp">（浜松球場・陸上競技場）</td>
+                                                        <td>約25分</td>
+                                                    </tr>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="sect-2">
+                            <div class="wrapper">
+                                <h2 class="def sawa">主要スポーツ会場への<br class="sp">アクセスも抜群！</h2>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="card">
+                                            <h3>お食事のご要望にお応えします <span></span></h3>
+                                            <div class="card-content">
+                                                <img src="/images/sports/sect_2_img_1.jpg" alt="">
+                                                <h4>お食事のご要望にお応えします</h4>
+                                                <p>ホテル料理人がご希望に沿ったお食事をご提案いたします。アレルギーやハラールなども対応可能ですのでご相談ください。</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card">
+                                            <h3>大型バス駐車場完備 <span></span></h3>
+                                            <div class="card-content">
+                                                <img src="/images/sports/sect_2_img_2.jpg" alt="">
+                                                <h4>大型バス駐車場完備</h4>
+                                                <p>大型バスが6台まで停められる駐車場を併設。最大200台収容の乗用車駐車場もあり。</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card">
+                                            <h3>ミーティングルームやマッサージ<br class="sp">ルーム<br class="pc">などご用意OK！ <span></span></h3>
+                                            <div class="card-content">
+                                                <img src="/images/sports/sect_2_img_3.jpg" alt="">
+                                                <h4>ミーティングルームやマッサージルーム<br>などご用意OK！</h4>
+                                                <p>お食事会場とは別にミーティングルームやマッサージルーム、トレーニングルームなどご用意可能です。</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card">
+                                            <h3>お部屋は200室以上！<br>フロア貸切も応相談！  <span></span></h3>
+                                            <div class="card-content">
+                                                <img src="/images/sports/sect_2_img_4.jpg" alt="">
+                                                <h4>お部屋は200室以上！<br>フロア貸切も応相談！ </h4>
+                                                <p>ツイン、シングルの他、トリプルルームもあり、幅広いご要望に対応可能です。</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card">
+                                            <h3>コリンランドリー完備！<span></span></h3>
+                                            <div class="card-content">
+                                                <img src="/images/sports/sect_2_img_5.jpg" alt="">
+                                                <h4>お部屋は200室以上！<br>フロア貸切も応相談！ </h4>
+                                                <p>館内にコインランドリーを完備！</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card">
+                                            <h3>コンビニ徒歩１分！<span></span></h3>
+                                            <div class="card-content">
+                                                <img src="/images/sports/sect_2_img_6.jpg" alt="">
+                                                <h4>コンビニ徒歩１分！</h4>
+                                                <p>24時間営業のコンビニエンスストアまで徒歩約1分。</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card">
+                                            <h3>ホテル隣は“出世城”<span></span></h3>
+                                            <div class="card-content">
+                                                <img src="/images/sports/sect_2_img_7.jpg" alt="">
+                                                <h4>ホテル隣は“出世城”</h4>
+                                                <p>ホテル隣にある浜松城は、徳川家康公が若き日を過ごしたお城で、別名「出世城」と呼ばれています。これで出世間違いなし⁉</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="sect-3 g_box01">
+                            <div class="wrapper">
+                                <div class="content">
+                                    <h2 class="bef_01 sawa">お問い合わせ</h2> 
+                                    <p>お電話またはメールにて、<br class="sp">お気軽にご相談ください。</p>
+                                    <div class="cta-wrap">  
+                                        <div>
+                                            <p class="sawa">tel: <a href="tel:053-487-0990" class="btnTel sawa">053-487-0990</a></p>
+                                            <span class="sawa">受付時間  9：00～18：30</span>
+                                        </div>
+                                        <a href="" class="btnContact">メールでお問い合わせ</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+					</div>
+                </div>
+                <!-- Main -->
+                <!--======================== //Main =====================================-->
+                <!--======================== SideBar =====================================-->
+                <div id="SideBar">
+                    <?php include($_SERVER['DOCUMENT_ROOT']."/inc/sidebar.php");?>
+                </div>
+                <!-- SideBar -->
+                <!--======================== //SideBar =====================================-->
+            </div>
+            <!-- Contents -->
+            <!--======================== //Contents ===============================================-->
+        </div>
+        <!-- AllBox -->
+        <!--======================== HeaderBox ===============================================-->
+        <div id="FooterBox">
+            <?php include($_SERVER['DOCUMENT_ROOT']."/inc/footer_box.php");?>
+        </div>
+        <!-- HeaderBox -->
+        <!--======================== //HeaderBox ===============================================-->
+        <?php include($_SERVER['DOCUMENT_ROOT']."/inc/tag/footer_tag.php");?>
+        <script src="/js/fs/jquery.flexslider-min.js" type="text/javascript"></script>
+        <link href="/js/fs/flexslider.css" media="all" rel="stylesheet">
+        <script type="text/javascript">
+            $('.card h3').click(function(){
+                $(this).toggleClass('active');
+                $(this).next('.card-content').slideToggle();
+            })
+        </script>
+    </body>
+</html>
